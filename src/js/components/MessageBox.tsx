68616f63a058456bdc285c022836251c2df722d4
Daniel Leśniak <dlesniak781@gmail.com>
2022-04-07 17:55:41 +0200

    Refactor MessageBox

--- src/js/components/MessageBox.tsx
+++ src/js/components/MessageBox.tsx
@@ -2,40 +2,41 @@ import React, { FC, useState, useEffect } from "react";
 
 interface Props {
     message: string
-    withSigns?: boolean
-
-}
-
-MessageBox.defaultProps = {
-    withSigns: false
+    withEllipsis?: boolean
+    ellipsisSign?: string
 }
 
 const MessageBox: FC<Props> = (props: Props) => {
-    const dots = useSign('.')
+    const dots = useEllipsis(props.ellipsisSign)
 
     return (
         <div className="flex justify-center items-center py-3 text-white text-xl">
-            {props.message + dots}
+            {props.message + (props.withEllipsis ? dots : '')}
         </div>
     )
 }
 
-function useSign(sigleSignValue: string, maxLength = 3) {
-    const [signs, setSigns] = useState(sigleSignValue)
+function useEllipsis(ellipsisSign: string, maxLength = 3) {
+    const [ellipsis, setEllipsis] = useState(ellipsisSign)
 
     useEffect(() => {
-        const signsTimer = setTimeout(() => {
-            if (signs.length === maxLength) {
-                setSigns(sigleSignValue)
+        const ellipsisTimer = setTimeout(() => {
+            if (ellipsis.length === maxLength) {
+                setEllipsis(ellipsisSign)
             } else {
-                setSigns(signs + sigleSignValue)
+                setEllipsis(ellipsis + ellipsisSign)
             }
         }, 1000)
 
-        return () => clearTimeout(signsTimer)
-    }, [signs, sigleSignValue, maxLength])
+        return () => clearTimeout(ellipsisTimer)
+    }, [ellipsis, ellipsisSign, maxLength])
 
-    return signs
+    return ellipsis
+}
+
+MessageBox.defaultProps = {
+    withEllipsis: true,
+    ellipsisSign: '.'
 }
 
 export default MessageBox
